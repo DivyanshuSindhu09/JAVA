9de50445209ec04bbfc2ef95fc7106114fec5e86
Divyanshu Sindhu <divyanshu.sindhu@gmail.com>
2025-09-19 14:20:13 +0530

    Add initial implementation of Primitives class demonstrating basic data types and wrapper classes

---
+++ basics/Primitives.java
@@ -0,0 +1,19 @@
+//! primitive is basically any data type which you can't break any further
+//! 'char' "string"
+
+public class Primitives {
+    public static void main(String[] args) {
+        int rollNo = 6;
+        //! few options
+        char letter = 'd';
+        float marks = 98.67f;
+        double largeDecimalNumbers = 456789.123456;
+        long largeInteger = 456789123L;
+        boolean check = true;
+
+        //! wrapper classes
+        Integer rno = 6; // 'I' is capital here
+        rno.hashCode();
+        //! many methods are there in wrapper classes
+    }
+}
