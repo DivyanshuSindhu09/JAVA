e0bc5764947fdbff3d45728dbff926ff0af8413d
Divyanshu Sindhu <divyanshu.sindhu@gmail.com>
2025-09-19 13:14:02 +0530

    starting with the basics

---
+++ basics/First.java
@@ -0,0 +1,14 @@
+//! every file with extension .java is a class
+//! jis name ki file hai us hi name ki class honi chahiye
+//! if you are creating a class make sure that it's first letter is capital
+//! jo file k name wali class hai wo public hogi
+//! public wali class co kahi se bhi access kr skte hain
+
+public class First{
+    public static void main(String[] args) {
+        System.out.println("Hello From Devansh Jaat!");
+    }
+}
+
+//? static keyword is used to call the method without creating the object of the class First
+//? void means nothing is returned from this method
